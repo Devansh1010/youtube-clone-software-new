a9210cd2b4c29a900c2ef306652fc76e66d07929
vivek prajapati <vivek.praj2912@gmail.com>
2025-07-21 22:39:43 +0530

    add user route
    validation is pending

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,57 @@
 import { asyncHandler } from '../uitls/asyncHandler.js'
+import User from '../models/user.model.js'
+import { ApiError } from '../uitls/ApiError.js'
+import { ApiResponse } from '../uitls/ApiResponse.js'
+import { uploadFileOnCloudinary } from '../uitls/cloudinary.js'
 
-export const registerUser = asyncHandler( async (req, res)=> {
-    res.status(200).json({
-        message: "Success"
+export const registerUser = asyncHandler(async (req, res) => {
+
+    const { username, fullname, password, email, } = await req.json()
+
+    // Validation data
+
+    //check user exist or not
+    const existUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
+
+    if (existUser) {
+        throw new ApiError(409, "Username and email already exist")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    console.log("file of multer: ", req.files)
+
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar image is required")
+    }
+
+    //upload to cloudinary
+    const avatar = await uploadFileOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadFileOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar image is required")
+    }
+
+    const User = await User.create({
+        fullname,
+        username: username.toLowercase(),
+        avatar: avatar.url,
+        coverImage: coverImage?.url || '',
+        email,
+        password
+    })
+
+    const userCreated = User.findById(User._id).select('-password -refreshToken')
+
+    if (!userCreated) {
+        throw new ApiError(504, "Something went wrong while creating user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, userCreated, "User registerd successfully")
+    )
 })
